175a1a16926d934b0edeb64d0551f5c0e389270d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2023-08-28 20:04:26 -0300

    Model Task

---
+++ ToDoXF/Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoXF.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
